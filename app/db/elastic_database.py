6061be4c2757acc212b871e0ab3087ec59b3c6eb
menachem cohen <770mc770@gmail.com>
2024-12-19 14:20:00 +0200

    models in mongo

--- app/db/elastic_database.py
+++ app/db/elastic_database.py
@@ -5,7 +5,6 @@ from elasticsearch import Elasticsearch
 # Load environment variables from .env file
 load_dotenv(verbose=True)
 
-
 # Create the Elasticsearch client
 es_client = Elasticsearch(
     [os.getenv("ELASTIC_URL_DB")],
