50e2ab34600360ed5776688fc1b5444833b7e3b1
menachem cohen <770mc770@gmail.com>
2024-12-22 12:20:00 +0200

    service insert

--- app/db/elastic_database.py
+++ app/db/elastic_database.py
@@ -5,6 +5,7 @@ from elasticsearch import Elasticsearch
 # Load environment variables from .env file
 load_dotenv(verbose=True)
 
+
 # Create the Elasticsearch client
 es_client = Elasticsearch(
     [os.getenv("ELASTIC_URL_DB")],
